e23109d70887dc1f92ed61c54a95437a4078f1f5
Shubham Mishra <55226462+rabbit-ninja@users.noreply.github.com>
2020-04-10 22:57:56 +0530

    added all files!
    its a simple and simplest project!

---
+++ final.php
@@ -0,0 +1,77 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$nameErr = "";
+$name = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $nameErr = "Name is required";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $nameErr = "Only letters and white space allowed";
+    }
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Congratulations!!! you answered all the 5 answers correctly!</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Name: <input type="text" name="name" value="<?php echo $name;?>">
+  <span class="error">* <?php echo $nameErr;?></span>
+  <br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+?>
+
+</body>
+</html>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "quiz";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO winner (name)
+VALUES ('$name')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
---
+++ question1.php
@@ -0,0 +1,61 @@
+<!DOCTYPE HTML>  
+<html>
+<head>
+<title>
+Question 1
+</title>
+<style>
+.error {color: #FF0000;}
+</style>
+</head>
+<body>  
+
+<?php
+// define variables and set to empty values
+$optionErr = "";
+$option = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["option"])) {
+    $optionErr = "Answer is required";
+  } else {
+    $option = test_input($_POST["option"]);
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+?>
+
+<h2>Question 1</h2>
+<p><span class="error">* required field</span></p>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
+  Options: <br>
+  <input type="radio" name="option" <?php if (isset($option) && $option=="option1") echo "checked";?> value="option1">option1 <br>
+  <input type="radio" name="option" <?php if (isset($option) && $option=="option2") echo "checked";?> value="option2">option2 <br>
+  <input type="radio" name="option" <?php if (isset($option) && $option=="option3") echo "checked";?> value="option3">option3 <br>
+  <input type="radio" name="option" <?php if (isset($option) && $option=="option4") echo "checked";?> value="option3">option4 <br>
+  <span class="error">* <?php echo $optionErr;?></span>
+  <br><br>
+  <input type="submit" name="submit" value="Submit">  
+</form>
+
+<?php
+echo "<h2>Your Input:</h2>";
+echo "<br>";
+if($option== "option1"){
+	echo $option;
+	echo "<br>";
+	echo "Awesome it's correct! you will be redirected to next page in 5 seconds!";
+	header( "refresh:5;url=question2.php" );
+}
+else
+	echo"Wrong answer!"
+?>
+
+</body>
+</html>
